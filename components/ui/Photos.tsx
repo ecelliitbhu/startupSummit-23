6e167b711bb110eb97f4ea4a83682bd825941bdb
Arun <arun.kumar.cd.civ21@itbhu.ac.in>
2023-08-13 21:57:47 +0530

    improved images for trailblazzers

--- components/ui/Photos.tsx
+++ components/ui/Photos.tsx
@@ -1,10 +1,10 @@
-import photo1 from "../../public/Trailblazzers/IIT_BHU_Uniforn_Founders/Amitva Saha- Xpressbees and FirstCry.png";
+import photo1 from "../../public/Trailblazzers/IIT_BHU_Uniforn_Founders/Amitva Saha- Xpressbees and FirstCry.jpg";
 import photo2 from "../../public/Trailblazzers/IIT_BHU_Uniforn_Founders/ElasticRun- Shitiz Bansal.png";
-import photo3 from "../../public/Trailblazzers/IIT_BHU_Uniforn_Founders/Jay-Chaudhry-ZScaler.jpeg";
+import photo3 from "../../public/Trailblazzers/IIT_BHU_Uniforn_Founders/Jay-Chaudhry-ZScaler.jpg";
 import photo4 from "../../public/Trailblazzers/IIT_BHU_Uniforn_Founders/Kailash Kailash - Zscaler.png";
 import photo5 from "../../public/Trailblazzers/IIT_BHU_Uniforn_Founders/OneCard- Anurag Sinha.png";
-import photo6 from "../../public/Trailblazzers/IIT_BHU_Uniforn_Founders/Prateek-Maheshwari-PW.png";
-import photo7 from "../../public/Trailblazzers/IIT_BHU_Uniforn_Founders/Sanjay Sethi- Shopclues.png";
+import photo6 from "../../public/Trailblazzers/IIT_BHU_Uniforn_Founders/Prateek-Maheshwari-PW.jpg";
+import photo7 from "../../public/Trailblazzers/IIT_BHU_Uniforn_Founders/Sanjay Sethi- Shopclues.jpg";
 import photo8 from "../../public/Trailblazzers/IIT_BHU_Uniforn_Founders/Sarvajna Dwivedi- Pearl Therapeutics.jpeg";
 import { StaticImageData } from "next/image";
 interface PersonProps {
@@ -12,7 +12,7 @@ interface PersonProps {
   company: string;
   quote?: string;
   imageUrl: StaticImageData;
-  position?:string;
+  position?: string;
 }
 export const images: PersonProps[] = [
   {
@@ -20,7 +20,7 @@ export const images: PersonProps[] = [
     company: "Xpressbees and FirstCry",
     quote: "Ecommerce isn't the cherry on the cake, it's the new cake.",
     imageUrl: photo1,
-    position:"Co-Founder & CEO at Xpressbees"
+    position: "Co-Founder & CEO at Xpressbees"
   },
 
   {
@@ -29,14 +29,14 @@ export const images: PersonProps[] = [
     quote:
       "Try to walk with the forward- thinking ,the dreamers,the believers,the courageous,the planners,the positive and the doers",
     imageUrl: photo3,
-    position:"Founder & CEO at Zscaler"
+    position: "Founder & CEO at Zscaler"
   },
   {
     name: "Anurag Sinha",
     company: "OneCard",
     quote: "When the pie becomes a cake, there is greater value for us to take",
     imageUrl: photo5,
-    position:"Co-Founder at OneCard"
+    position: "Co-Founder at OneCard"
   },
   {
     name: "Prateek Maheshwari",
@@ -44,14 +44,14 @@ export const images: PersonProps[] = [
     quote:
       "Being the most thoughtful person in the room is only possible if you are in the right room",
     imageUrl: photo6,
-    position:"Co-Founder at PW"
+    position: "Co-Founder at PW"
   },
   {
     name: "Sanjay Sethi",
     company: "Shopclues",
     quote: "To be a  disrupter, You Dont Have to be an Asshole",
     imageUrl: photo7,
-    position:"Founder & CEO at Shopclues"
+    position: "Founder & CEO at Shopclues"
   },
 
   {
@@ -60,6 +60,6 @@ export const images: PersonProps[] = [
     quote:
       "Perseverance, inventiveness and initiative are essential traits for a researcher",
     imageUrl: photo8,
-    position:"Co-Founder at Pearl Therapeutics"
+    position: "Co-Founder at Pearl Therapeutics"
   },
 ];
