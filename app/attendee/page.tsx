9464b0be8d59243ceaeed1092d9117cd05b18545
rahulX001 <rahul.kumarsonkar.eee21@itbhu.ac.in>
2023-08-24 16:35:24 +0530

    responsive of  payment

--- app/attendee/page.tsx
+++ app/attendee/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { EventEmbed } from "@/components/payment";
 const Page = () => {
   return (
-    <div className={"pt-32  w-[100vw-2rem] my-2  flex justify-center items-center min-h-screen"}>
+    <div className={"  w-[100vw-2rem] my-2  flex justify-center items-center min-h-screen"}>
       <EventEmbed />
     </div>
   );
